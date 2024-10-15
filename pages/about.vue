8721a988ce6e19d90851ed6c5b32845d8e8da71d
oleg <oleg81_404@mail.ru>
2024-10-15 21:25:30 +0300

    init

---
+++ pages/about.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Hallo about</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Hallo index</h1>
+    </div>
+</template>
+
+<script setup>
+</script>
